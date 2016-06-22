e6b8f5da3284e7be7226d002dd3714f5cd541248
Natasha <natasha@jadavji.ca>
2016-06-21 21:43:11 -0700

    Fizzbuzz method
    Implemented fizzbuzz method that takes in starting and finishing number
    so that we can programmatically change the numbers

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,22 @@
-(1..100).each do |i|
-      if i % 5 == 0 and i % 3 == 0
-        puts "FizzBuzz"
-      elsif i % 5 == 0
-        puts "Buzz"
-      elsif i % 3 == 0
-        puts "Fizz"
-      else
-        puts i
-      end
+def fizzbuzz(s,f)
+  s.upto(f) { |i| }
+  puts i
+  fizzbuzz.each do |i|
+    if i % 15
+      puts "FizzBuzz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    else
+      puts i
     end
+  end
+end
+
+fizzbuzz(15, 225)
+
+
+
+
+
